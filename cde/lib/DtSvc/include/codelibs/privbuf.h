67affa6f3155ea33a922f1147f1c43ef407b8622
Peter Howkins <flibble@users.sourceforge.net>
2012-03-11 18:15:12 +0000

    Add linux to list of platforms that are object to those definitions

--- cde/lib/DtSvc/include/codelibs/privbuf.h
+++ cde/lib/DtSvc/include/codelibs/privbuf.h
@@ -48,7 +48,7 @@ typedef void (*privbuf_func)(void *v);
 #include <stddef.h>
 #include <stdlib.h>
 
-#if defined(__osf__) || defined(sun)
+#if defined(__osf__) || defined(sun) || defined(linux)
 // The DEC C++ compiler rejects valid inline declarations, claiming
 // they have both internal and external linkage.
 #else
