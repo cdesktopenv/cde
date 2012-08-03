cf410afbdb9c29def503b689f548dca339ceac24
Jon Trulson <jon@radscan.com>
2012-08-03 15:06:56 -0600

    dtbuilder: fixup TOG copyrights issue in generated files.
    The TOG copyrights were being removed after a rebuild, leaving behind
    the scary "RESTRICTED" copyright text that was originally there.
    
    The issue was that the TOG copyrights were not properly embedded
    within a 'DTB_USER_CODE_START' code block.
    
    dtcodegen does not preserve any code outside DT_USER_CODE START and
    END blocks.
    
    Additionally, these objects are built with -merge by dtcodgen, so the
    existing 'RESTRICTED' header within the codeblocks was being retained.

--- cde/programs/dtappbuilder/src/ab/cpanel_stubs.c
+++ cde/programs/dtappbuilder/src/ab/cpanel_stubs.c
@@ -1,3 +1,4 @@
+/*** DTB_USER_CODE_START vvv Add file header below vvv ***/
 /*
  * CDE - Common Desktop Environment
  *
@@ -20,26 +21,6 @@
  * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
  * Floor, Boston, MA 02110-1301 USA
  */
-/*** DTB_USER_CODE_START vvv Add file header below vvv ***/
-/*
- *      $XConsortium: cpanel_stubs.c /main/4 1996/04/18 13:17:13 drk $
- *
- * @(#)cpanel_stubs.c	1.5 27 Sep 1994 cde_app_builder/src/ab
- *
- *      RESTRICTED CONFIDENTIAL INFORMATION:
- *
- *      The information in this document is subject to special
- *      restrictions in a confidential disclosure agreement between
- *      HP, IBM, Sun, USL, SCO and Univel.  Do not distribute this
- *      document outside HP, IBM, Sun, USL, SCO, or Univel without
- *      Sun's specific written approval.  This document and all copies
- *      and derivative works thereof must be returned or destroyed at
- *      Sun's request.
- *
- *      Copyright 1993 Sun Microsystems, Inc.  All rights reserved.
- *
- */
-
 /*** DTB_USER_CODE_END   ^^^ Add file header above ^^^ ***/
 
 /*
