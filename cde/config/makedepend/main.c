18dd384068910c496d03f67f7b81e8472ac897fb
Pascal Stumpf <Pascal.Stumpf@cubes.de>
2012-08-09 21:35:12 +0200

    Rename getline() to not conflict with the standard POSIX interface of the same name.

--- cde/config/makedepend/main.c
+++ cde/config/makedepend/main.c
@@ -499,7 +499,7 @@ match(str, list)
  * Get the next line.  We only return lines beginning with '#' since that
  * is all this program is ever interested in.
  */
-char *getline(filep)
+char *our_getline(filep)
 	register struct filepointer	*filep;
 {
 	register char	*p,	/* walking pointer */
