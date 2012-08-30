ef67cebea54864cd196029c4ca94a4b65fce5d0d
Peter Howkins <flibble@users.sourceforge.net>
2012-08-30 18:25:49 +0100

    dtfile: Resolve implicit imcompatible declaration warnings.

--- cde/programs/dtfile/dtcopy/overwrtdialog.c
+++ cde/programs/dtfile/dtcopy/overwrtdialog.c
@@ -46,6 +46,7 @@
  ************************************<+>*************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/wait.h>
