cdesktopenv/cde

commit ca9da25d44b258d8ca933808d24668cddd727449

dtcm: Resolve all -Wformat-security warnings.

cde/programs/dtcm/dtcm/misc.c
@@ -887,55 +887,55 @@ editor_err_msg(Widget frame, char *name, Validate_op op, Pixmap p) {
 
 	switch(op) {
 	case COULD_NOT_OPEN_FILE:
-		sprintf(buf, catgets(c->DT_catd, 1, 355, "Unable to open callog file.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 355, "Unable to open callog file.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, CMSD_ERROR_HELP);
 		break;
 	case CANCEL_APPT:
-		sprintf(buf, catgets(c->DT_catd, 1, 356, "Per user request, Operation was cancelled.\nCalendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 356, "Per user request, Operation was cancelled.\nCalendar"));
 		help_button = False;
 		break;
 	case INVALID_DATE:
-		sprintf(buf, catgets(c->DT_catd, 1, 357, "Invalid value in DATE field.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 357, "Invalid value in DATE field.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, DATE_ERROR_HELP);
 		break;
 	case INVALID_START:
-		sprintf(buf, catgets(c->DT_catd, 1, 358, "Invalid value in START field.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 358, "Invalid value in START field.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, START_ERROR_HELP);
 		break;
 	case INVALID_TIME_DUE:
-		sprintf(buf, catgets(c->DT_catd, 1, 1011, "Invalid value in the \"Time Due\" field.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 1011, "Invalid value in the \"Time Due\" field.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, START_ERROR_HELP);
 		break;
 	case INVALID_STOP:
-		sprintf(buf, catgets(c->DT_catd, 1, 359, "Invalid value in END field.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 359, "Invalid value in END field.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, STOP_ERROR_HELP);
 		break;
 	case MISSING_DATE:
-		sprintf(buf, catgets(c->DT_catd, 1, 360, "You must provide a DATE value.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 360, "You must provide a DATE value.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, MISSING_FIELD_ERROR_HELP);
 		break;
 	case MISSING_START:
-		sprintf(buf, catgets(c->DT_catd, 1, 361, "You must provide a START value.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 361, "You must provide a START value.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, MISSING_FIELD_ERROR_HELP);
 		break;
 	case MISSING_WHAT:
-		sprintf(buf, catgets(c->DT_catd, 1, 362, "You must provide a WHAT value.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 362, "You must provide a WHAT value.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, MISSING_FIELD_ERROR_HELP);
 		break;
 	case REPEAT_FOR_MISMATCH:
-		sprintf(buf, catgets(c->DT_catd, 1, 363, "Invalid or mismatched REPEAT and FOR values.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 363, "Invalid or mismatched REPEAT and FOR values.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, REPEAT_FOR_ERROR_HELP);
 		break;
 	case INVALID_NOTIME_APPT:
-		sprintf(buf, catgets(c->DT_catd, 1, 724, "No Time appointments must have a WHAT value specified.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 724, "No Time appointments must have a WHAT value specified.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, NO_TIME_ERROR_HELP);
 		break;
 	case INVALID_TIME:
-		sprintf(buf, catgets(c->DT_catd, 1, 1104, "Invalid value in TIME DUE field.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 1104, "Invalid value in TIME DUE field.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, START_ERROR_HELP);
 		break;
 	case MISSING_TIME:
-		sprintf(buf, catgets(c->DT_catd, 1, 1105, "You must provide a TIME DUE value.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 1105, "You must provide a TIME DUE value.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, MISSING_FIELD_ERROR_HELP);
 		break;
 	case VALID_APPT:
@@ -990,75 +990,75 @@ backend_err_msg(
 
 	switch(stat) {
 	case CSA_E_CALENDAR_EXISTS:
-		sprintf(buf, catgets(c->DT_catd, 1, 367, "Calendar already exists.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 367, "Calendar already exists.\nOperation was cancelled.  Calendar"));
 		help_button = False;
 		break;
 	case CSA_E_CALENDAR_NOT_EXIST:
-		sprintf(buf, catgets(c->DT_catd, 1, 368, "Calendar does not exist.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 368, "Calendar does not exist.\nOperation was cancelled.  Calendar"));
 		help_button = False;
 		break;
 	case CSA_X_DT_E_ENTRY_NOT_FOUND:
-		sprintf(buf, catgets(c->DT_catd, 1, 725, "Calendar Entry does not exist.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 725, "Calendar Entry does not exist.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_INVALID_ENTRY_HANDLE:
-		sprintf(buf, catgets(c->DT_catd, 1, 881, "Internal Error #1: Invalid calendar/entry handle.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 881, "Internal Error #1: Invalid calendar/entry handle.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_INVALID_SESSION_HANDLE:
-		sprintf(buf, catgets(c->DT_catd, 1, 882, "Internal Error #2: Invalid session handle.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 882, "Internal Error #2: Invalid session handle.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_NO_AUTHORITY:
-		sprintf(buf, catgets(c->DT_catd, 1, 370, "Authorization error.  Permission denied.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 370, "Authorization error.  Permission denied.\nOperation was cancelled.  Calendar"));
 		help_button = False;
 		break;
 	case CSA_E_INVALID_PARAMETER:
-		sprintf(buf, catgets(c->DT_catd, 1, 883, "Internal Error #3: Invalid data value.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 883, "Internal Error #3: Invalid data value.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_READONLY:
-		sprintf(buf, catgets(c->DT_catd, 1, 884, "Internal Error #4: Value specified for a read-only attribute.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", atgets(c->DT_catd, 1, 884, "Internal Error #4: Value specified for a read-only attribute.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_INVALID_ATTRIBUTE_VALUE:
-		sprintf(buf, catgets(c->DT_catd, 1, 885, "Internal Error #5: Incorrect data type specified for an attribute value.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 885, "Internal Error #5: Incorrect data type specified for an attribute value.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_UNSUPPORTED_ATTRIBUTE:
-		sprintf(buf, catgets(c->DT_catd, 1, 886, "Internal Error #6: Specified attribute is not supported on this calendar version.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 886, "Internal Error #6: Specified attribute is not supported on this calendar version.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_NOT_SUPPORTED:
-		sprintf(buf, catgets(c->DT_catd, 1, 887, "Internal Error #7: Specified function is not supported on this calendar version.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 887, "Internal Error #7: Specified function is not supported on this calendar version.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_INVALID_ENUM:
-		sprintf(buf, catgets(c->DT_catd, 1, 888, "Internal Error #8: Invalid operator specified.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 888, "Internal Error #8: Invalid operator specified.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_INSUFFICIENT_MEMORY:
-		sprintf(buf, catgets(c->DT_catd, 1, 378, "Memory allocation error - not enough memory.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 378, "Memory allocation error - not enough memory.\nOperation was cancelled.  Calendar"));
 		sprintf(error_buf, MEMORY_ALLOC_ERROR_HELP);
 		break;
 	case CSA_E_DISK_FULL:
-		sprintf(buf, catgets(c->DT_catd, 1, 907, "No space left for calendar file.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 907, "No space left for calendar file.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_SERVICE_UNAVAILABLE:
-		sprintf(buf, catgets(c->DT_catd, 1, 921, "Couldn't access calendar: "));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 921, "Couldn't access calendar: "));
 		break;
 	case CSA_X_DT_E_INVALID_SERVER_LOCATION:
-		sprintf(buf, catgets(c->DT_catd, 1, 641, "Server failed to get network address\nfor the specified location:  invalid hostname."));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 641, "Server failed to get network address\nfor the specified location:  invalid hostname."));
 		break;
 	case CSA_X_DT_E_SERVICE_NOT_REGISTERED:
-		sprintf(buf, catgets(c->DT_catd, 1, 642, "rpc.cmsd daemon is not registered.\nPlease verify rpc.cmsd is installed correctly on your system."));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 642, "rpc.cmsd daemon is not registered.\nPlease verify rpc.cmsd is installed correctly on your system."));
 		sprintf(error_buf, CMSD_ERROR_HELP);
 		break;
 	case CSA_X_DT_E_SERVER_TIMEOUT:
-		sprintf(buf, catgets(c->DT_catd, 1, 727, "Your request timed out\nPlease verify rpc.cmsd is installed correctly on your system."));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 727, "Your request timed out\nPlease verify rpc.cmsd is installed correctly on your system."));
 		sprintf(error_buf, CMSD_ERROR_HELP);
 		break;
 	case CSA_E_FAILURE:
-		sprintf(buf, catgets(c->DT_catd, 1, 643, "Couldn't access calendar. Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 643, "Couldn't access calendar. Calendar"));
 		break;
 	case CSA_E_INVALID_RULE:
-		sprintf(buf, catgets(c->DT_catd, 1, 1120, "The event cannot be scheduled in the given time frame.\nThe operation was cancelled."));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 1120, "The event cannot be scheduled in the given time frame.\nThe operation was cancelled."));
 		break;
 	case CSA_SUCCESS:
 		return;
 	default:
-		sprintf(buf, catgets(c->DT_catd, 1, 383, "Unknown error.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 383, "Unknown error.\nOperation was cancelled.  Calendar"));
 		break;
 	}
 

cde/programs/dtcm/dtcm/alarm.c
@@ -218,7 +218,7 @@ mail_it(XtPointer client_data, XtIntervalId *interval_id, CSA_reminder_reference
 	    (appt->mail->value->item.reminder_value->reminder_data.size == 0)) {
 		/* empty recipient */
 		if (debug)
-			fprintf(stderr, catgets(c->DT_catd, 1, 1,
+			fprintf(stderr, "%s", catgets(c->DT_catd, 1, 1,
 				"dtcm: empty recipient in mail reminder\n"));
 		to = appt->author->value->item.calendar_user_value->user_name;
 	} else {

cde/programs/dtcm/dtcm/calendarA.c
@@ -1947,7 +1947,7 @@ quit_handler(Widget w, XtPointer cdata, XtPointer cbs) {
 	Calendar	*c = (Calendar *)cdata;
 
 	if (debug)
-	  fprintf(stderr, catgets(c->DT_catd, 1, 89, "in quit_handler\n"));
+	  fprintf(stderr, "%s", catgets(c->DT_catd, 1, 89, "in quit_handler\n"));
 
 	if (editor_showing((Editor*) c->editor) ||
 	    todo_showing((ToDo*) c->todo))
@@ -3136,7 +3136,7 @@ switch_it(Calendar *c, char *new_calendar, WindowType win)
 	} else {
 		if ((user = cm_target2name(new_calendar)) == NULL) {
 			if (!strcmp(new_calendar, ""))
-				sprintf(buf, catgets(c->DT_catd, 1, 619, "Please enter a calendar name in the format: <user>@<hostname>"));
+				sprintf(buf, "%s", catgets(c->DT_catd, 1, 619, "Please enter a calendar name in the format: <user>@<hostname>"));
 			else
 				sprintf(buf, catgets(c->DT_catd, 1, 620,
 		"Unknown calendar. Calendar name needed: <name>%s"),
@@ -3149,7 +3149,7 @@ switch_it(Calendar *c, char *new_calendar, WindowType win)
 
 		if ((loc = cm_target2location(new_calendar)) == NULL) {
 			if (!strcmp(new_calendar, ""))
-				sprintf(buf, catgets(c->DT_catd, 1, 619, "Please enter a calendar name in the format: <user>@<hostname>"));
+				sprintf(buf, "%s", catgets(c->DT_catd, 1, 619, "Please enter a calendar name in the format: <user>@<hostname>"));
 			else
 				sprintf(buf, catgets(c->DT_catd, 1, 622,
 	"Unknown calendar. Hostname needed: %s@<hostname>"),

cde/programs/dtcm/dtcm/cm_insert.c
@@ -145,8 +145,8 @@ cm_args(int argc, char **argv)
 			argv = grab(++argv,cm_appt_file,'-');
 			break;
 		default:
-			fprintf(stderr, catgets(DT_catd, 1, 191, "Usage:\n\tdtcm_insert [ -c calendar ] [-d <mm/dd/yy>] [ -v view ]\n"));
-			fprintf(stderr, catgets(DT_catd, 1, 192, "                   [-w what string] [-s <HH:MMam/pm>] [-e <HH:MMam/pm>]\n"));
+			fprintf(stderr, "%s", catgets(DT_catd, 1, 191, "Usage:\n\tdtcm_insert [ -c calendar ] [-d <mm/dd/yy>] [ -v view ]\n"));
+			fprintf(stderr, "%s", catgets(DT_catd, 1, 192, "                   [-w what string] [-s <HH:MMam/pm>] [-e <HH:MMam/pm>]\n"));
 			exit(1);
 		}
 	}
@@ -179,7 +179,7 @@ prompt_for_insert(Props *p) {
         format_tick(now(), get_int_prop(p, CP_DATEORDERING),
 		    get_int_prop(p, CP_DATESEPARATOR), date_str);
 
-	printf(catgets(DT_catd, 1, 193, "Please enter the information for the appointment you wish to add.\nDefaults will be shown in parentheses.\n"));
+	printf("%s", catgets(DT_catd, 1, 193, "Please enter the information for the appointment you wish to add.\nDefaults will be shown in parentheses.\n"));
 	prompt_for_line(catgets(DT_catd, 1, 194, 
 		"Calendar (%s): "), cm_get_credentials(), cm_target);
 	prompt_for_line(catgets(DT_catd, 1, 195, 
@@ -195,7 +195,7 @@ prompt_for_insert(Props *p) {
 			if (valid_time(p, timecopy))
 				valid = TRUE;
 			else
-				printf(catgets(DT_catd, 1, 197, "You have entered an invalid time.  Please try again:\n"));
+				printf("%s", catgets(DT_catd, 1, 197, "You have entered an invalid time.  Please try again:\n"));
 			free(timecopy);
 		}
 	}
@@ -218,12 +218,12 @@ prompt_for_insert(Props *p) {
 			"Repeat (%s): "), cm_repeatstr, cm_repeatstr);
 
 	if (strcmp(cm_repeatstr, catgets(DT_catd, 1, 200, "One Time"))) {
-		sprintf(buf, catgets(DT_catd, 1, 203, "no default"));
+		sprintf(buf, "%s", catgets(DT_catd, 1, 203, "no default"));
 		prompt_for_line(
 			catgets(DT_catd, 1, 204, "For (%s): "), buf, cm_for);
 	}
 
-	printf(catgets(DT_catd, 1, 205, 
+	printf("%s", catgets(DT_catd, 1, 205, 
 		"What (you may enter up to 5 lines, use ^D to finish):\n"));
 	cm_what[0] = NULL;
 	for (index = 0; index < 5; index++)

cde/programs/dtcm/dtcm/dnd.c
@@ -636,7 +636,7 @@ GetIcon(Calendar *calendar)
                         drag_xbm_width, drag_xbm_height);
                 if (e->drag_bitmap == NULL) {
 
-                        printf(catgets(calendar->DT_catd, 1, 237, "XCreateBitmapFromData() failed for bitmap.\n"));
+                        printf("%s", catgets(calendar->DT_catd, 1, 237, "XCreateBitmapFromData() failed for bitmap.\n"));
                         return;
                 }
                 else
@@ -647,7 +647,7 @@ GetIcon(Calendar *calendar)
                         window, (char *) drag_mask_xbm_bits,
                         drag_mask_xbm_width, drag_mask_xbm_height);
                 if (e->drag_mask == NULL) {
-                        printf(catgets(calendar->DT_catd, 1, 238, "XCreateBitmapFromData() failed for mask.\n"));
+                        printf("%s", catgets(calendar->DT_catd, 1, 238, "XCreateBitmapFromData() failed for mask.\n"));
                         return;
                 }
                 else
@@ -798,7 +798,7 @@ TranslationDragStart(
 			     DtNsourceIcon, e->drag_icon,
 			     NULL) == NULL) {
  
-                printf(catgets(calendar->DT_catd, 1, 239, 
+                printf("%s", catgets(calendar->DT_catd, 1, 239, 
 					"DragStart returned NULL.\n"));
         }
 }
@@ -948,7 +948,7 @@ ApptDragStart(
 		NULL)
             == NULL) {
  
-                printf(catgets(calendar->DT_catd, 1, 239,
+                printf("%s", catgets(calendar->DT_catd, 1, 239,
 			       "DragStart returned NULL.\n"));
         }
 }

cde/programs/dtcm/dtcm/find.c
@@ -588,7 +588,7 @@ XmPushButtonCallbackStruct *cbs;
 	astr = XmTextGetString(f->apptstr);
 
         if (astr == NULL || *astr == NULL) {
-		sprintf(message, catgets(c->DT_catd, 1, 290, "Specify Appt String to Match."));
+		sprintf(message, "%s", catgets(c->DT_catd, 1, 290, "Specify Appt String to Match."));
 		set_message(f->find_message, message);
                 return;
         }
@@ -613,12 +613,12 @@ XmPushButtonCallbackStruct *cbs;
 		if (start == DATE_BBOT)
 			start = get_bot();
 		else if (start == DATE_AEOT) {
-                        sprintf(message,  catgets(c->DT_catd, 1, 810, "Please enter a start date after 1/1/1970"));
+                        sprintf(message, "%s", catgets(c->DT_catd, 1, 810, "Please enter a start date after 1/1/1970"));
                         set_message(f->find_message, message);
                         return;
                 }
 		else if (start <= 0) {
-                        sprintf(message,  catgets(c->DT_catd, 1, 811, "Malformed start date"));
+                        sprintf(message, "%s", catgets(c->DT_catd, 1, 811, "Malformed start date"));
                         set_message(f->find_message, message);
                         return;
                 }
@@ -629,18 +629,18 @@ XmPushButtonCallbackStruct *cbs;
 		if (end_of_time == DATE_AEOT)
 			end_of_time = real_eot;
 		else if (end_of_time == DATE_BBOT) {
-                        sprintf(message,  catgets(c->DT_catd, 1, 812, "Please enter an end date before 1/1/2038"));
+                        sprintf(message, "%s", catgets(c->DT_catd, 1, 812, "Please enter an end date before 1/1/2038"));
                         set_message(f->find_message, message);
                         return;
                 }
 		else if (end_of_time <= 0) {
-                        sprintf(message,  catgets(c->DT_catd, 1, 813, "Malformed end date"));
+                        sprintf(message, "%s", catgets(c->DT_catd, 1, 813, "Malformed end date"));
                         set_message(f->find_message, message);
                         return;
                 }
 
 		if (start >= end_of_time) {
-			sprintf(message, catgets(c->DT_catd, 1, 713, "You must choose a begin date before the end date."));
+			sprintf(message, "%s", catgets(c->DT_catd, 1, 713, "You must choose a begin date before the end date."));
                 	set_message(f->find_message, message);
                 	return;
         	}
@@ -752,7 +752,7 @@ XmPushButtonCallbackStruct *cbs;
         }  /* end for range.end loop */
 
 	if (match_total == 0)
-		sprintf(message, catgets(c->DT_catd, 1, 291, "Appointment Not Found."));
+		sprintf(message, "%s", catgets(c->DT_catd, 1, 291, "Appointment Not Found."));
 	else if (match_total == 1)
 		sprintf(message, catgets(c->DT_catd, 1, 631, "%d match found"), match_total);
 	else

cde/programs/dtcm/dtcm/goto.c
@@ -259,7 +259,7 @@ goto_date(Widget widget, XtPointer client_data, XmPushButtonCallbackStruct *cbs)
  
 	date = XmTextGetString(g->datetext);
 	if ((*date == NULL) || (date == NULL)) {
-		sprintf(message, catgets(c->DT_catd, 1, 297, "Please type in a date"));
+		sprintf(message, "%s", catgets(c->DT_catd, 1, 297, "Please type in a date"));
 		set_message(g->goto_message, message);
                 return;
 	}
@@ -279,17 +279,17 @@ goto_date(Widget widget, XtPointer client_data, XmPushButtonCallbackStruct *cbs)
  
 
 	if (gotodate == DATE_BBOT) {
-		sprintf(message, catgets(c->DT_catd, 1, 814, "You must enter a date after 1969 and before 2038"));
+		sprintf(message, "%s", catgets(c->DT_catd, 1, 814, "You must enter a date after 1969 and before 2038"));
 		set_message(g->goto_message, message);
                 return;
 	}
 	else if (gotodate == DATE_AEOT) {
-		sprintf(message, catgets(c->DT_catd, 1, 814, "You must enter a date after 1969 and before 2038"));
+		sprintf(message, "%s", catgets(c->DT_catd, 1, 814, "You must enter a date after 1969 and before 2038"));
 		set_message(g->goto_message, message);
                 return;
 	}
         else if (gotodate <= 0) {
-		sprintf(message, catgets(c->DT_catd, 1, 299, "Invalid Date"));
+		sprintf(message, "%s", catgets(c->DT_catd, 1, 299, "Invalid Date"));
 		set_message(g->goto_message, message);
                 return;
         }

cde/programs/dtcm/dtcm/help.c
@@ -153,7 +153,7 @@ void show_main_help(Widget w, XtPointer clientData, XtPointer cbs)
                switch ((int)status)
                   {
                     case DtHELP_SELECT_ERROR:
-                      printf(catgets(c->DT_catd, 1, 350, "Selection Error, cannot continue\n"));
+                      printf("%s", catgets(c->DT_catd, 1, 350, "Selection Error, cannot continue\n"));
                     break;
                     case DtHELP_SELECT_VALID:
                       while (selWidget != NULL)

commit 4620bdb0db2356aa24eab02ec7e51255e3c76a30

dtcm: fix typo introduced by commit ca9da25d44b258d8ca933808d24668cddd727449

cde/programs/dtcm/dtcm/misc.c
@@ -1014,7 +1014,7 @@ backend_err_msg(
 		sprintf(buf, "%s", catgets(c->DT_catd, 1, 883, "Internal Error #3: Invalid data value.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_READONLY:
-		sprintf(buf, "%s", atgets(c->DT_catd, 1, 884, "Internal Error #4: Value specified for a read-only attribute.\nOperation was cancelled.  Calendar"));
+		sprintf(buf, "%s", catgets(c->DT_catd, 1, 884, "Internal Error #4: Value specified for a read-only attribute.\nOperation was cancelled.  Calendar"));
 		break;
 	case CSA_E_INVALID_ATTRIBUTE_VALUE:
 		sprintf(buf, "%s", catgets(c->DT_catd, 1, 885, "Internal Error #5: Incorrect data type specified for an attribute value.\nOperation was cancelled.  Calendar"));