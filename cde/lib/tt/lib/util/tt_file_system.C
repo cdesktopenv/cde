5669c8a60f67cc35c58d201e982dccdc4c074592
alx <alx@fastestcode.org>
2017-07-29 15:10:54 +0200

    libtt: pass proper flags to getfsstat, log error message and exit if it fails

--- cde/lib/tt/lib/util/tt_file_system.C
+++ cde/lib/tt/lib/util/tt_file_system.C
@@ -46,6 +46,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #if defined(linux)
 # include <sys/poll.h>
 #else
@@ -516,8 +517,11 @@ updateFileSystemEntries ()
         int             flags = MNT_NOWAIT;
         char            *s, *host, path[MNAMELEN] ;
  
-        numfs = getfsstat ( (struct statfs *)0, 0, 0 );
-
+        numfs = getfsstat ( (struct statfs *)0, 0, flags );
+        if(numfs == (-1)){
+            _tt_syslog(0,LOG_ERR,"getfsstat: %s",strerror(errno));
+            exit(EXIT_FAILURE);
+        }
         bufsize = numfs * sizeof ( struct statfs );
         buf = (struct statfs *) malloc ( bufsize );
 	memset ((void *)buf,0,bufsize);
