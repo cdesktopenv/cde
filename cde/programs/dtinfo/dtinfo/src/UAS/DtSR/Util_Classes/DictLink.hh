3ccf3a8b86d9eef328f4dfd80da3eb013bd3d9ef
chase <chase@localhost.com>
2018-05-12 09:36:25 -0700

    Add missing license headers on *.hh files and others

--- cde/programs/dtinfo/dtinfo/src/UAS/DtSR/Util_Classes/DictLink.hh
+++ cde/programs/dtinfo/dtinfo/src/UAS/DtSR/Util_Classes/DictLink.hh
@@ -1,3 +1,25 @@
+/*
+ * CDE - Common Desktop Environment
+ *
+ * Copyright (c) 1993-2012, The Open Group. All rights reserved.
+ *
+ * These libraries and programs are free software; you can
+ * redistribute them and/or modify them under the terms of the GNU
+ * Lesser General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * These libraries and programs are distributed in the hope that
+ * they will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with these libraries and programs; if not, write
+ * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301 USA
+ */
 // $XConsortium: DictLink.hh /main/4 1996/06/11 16:42:36 cde-hal $
 #ifndef _TK_ASSOCIATIVE_ARRAY_DICTLINK_HH_
 #define _TK_ASSOCIATIVE_ARRAY_DICTLINK_HH_
