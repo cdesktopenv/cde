a047b0be0ed2d3d19952381a8b31360e8e430712
Peter Howkins <flibble@users.sf.net>
2018-07-13 18:35:25 +0100

    dtcm: Coverity 89595

--- cde/programs/dtcm/server/rtable3.c
+++ cde/programs/dtcm/server/rtable3.c
@@ -233,11 +233,13 @@ _DtCm_rtable_change_3_svc(Table_Args_3 *args, struct svc_req *svcrq)
 
         if (res!=NULL) xdr_free ((xdrproc_t)_DtCm_xdr_Table_Res_3, (char*)res); res = NULL; 
 
-        newargs = _DtCm_tableargs3_to_tableargs4(args); 
-        newres = _DtCm_rtable_change_4_svc(newargs, svcrq); 
-        res = _DtCm_tableres4_to_tableres3(newres);  
+        newargs = _DtCm_tableargs3_to_tableargs4(args);
+        if(newargs) {
+                newres = _DtCm_rtable_change_4_svc(newargs, svcrq);
+                res = _DtCm_tableres4_to_tableres3(newres);
 
-        if (newargs!=NULL) xdr_free((xdrproc_t)_DtCm_xdr_Table_Args_4, (char*)newargs);  
+                xdr_free((xdrproc_t)_DtCm_xdr_Table_Args_4, (char*)newargs);
+        }
 
         return(res); 
 }
