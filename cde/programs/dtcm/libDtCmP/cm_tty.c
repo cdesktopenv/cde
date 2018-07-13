b260bc25d5d144af5fc8c260670f43493d26abb4
Peter Howkins <flibble@users.sf.net>
2018-07-13 17:13:18 +0100

    dtcm: Coverity 89143

--- cde/programs/dtcm/libDtCmP/cm_tty.c
+++ cde/programs/dtcm/libDtCmP/cm_tty.c
@@ -1261,7 +1261,7 @@ generate_recurrence_rule(Dtcm_appointment *appt, int version) {
                          rule_buf1[32],
                          rule_buf2[32];
 	CSA_sint32	repeat_type;
-	CSA_uint32	repeat_nth;
+	CSA_uint32	repeat_nth = 0;
 	CSA_uint32	repeat_for = 0;
 	Tick		appt_time;
 	int     	wk;
