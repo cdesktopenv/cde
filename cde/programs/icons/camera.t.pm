2f6f1d2afbaea1487da35c2f9d514ad878bcd35a
Jon Trulson <jon@radscan.com>
2018-07-19 18:39:56 -0600

    Finish integrating Antonis's desktop_approots work
    In addition:
    
    - moved video type detection into a separate file: videoTypes.dt out
      of datatypes.dt.  Also, removed all actions from Antonis' dt files,
      as these are handled by the new actions (listed below).
    
      Image, postscript, and PDF types definitions are still located in
      datatypes.dt.src
    
    - supports most image files, PDFs, postscript files, and video files
      via dtapp now.
    
    - Added new actions:
    
      - DisplayImage
      - DisplayVideo
      - DisplayPDF
      - DisplayPS
    
      All of these call the dtapp_* helpers to locate an appropriate
      program to handle the task.
    
    - Added a camera icon
    
    - standardized the icons used to display the various types - all
      images use the Dtimage icon now, as an example.
    
    - moved the new *.dt files to their proper place in programs/types,
      rather than programs/localized/C/types (my bad).
    
    Please add any further video types into videoTypes.dt, and everything
    else (for now) into datatypes.dt.
    
    In the future, we should probably separate these out into
    type-specific .dt files.  Also, something should be done about playing
    audio files too (maybe DisplayVideo.dt can handle all those too?)

---
+++ cde/programs/icons/camera.t.pm
@@ -0,0 +1,29 @@
+/* XPM */
+static char * IMdisplay_t_pm[] = {
+/* width height ncolors cpp [x_hot y_hot] */
+"16 16 7 1 0 0",
+/* colors */
+" 	s none	m none	c none",
+".	s iconColor1	m black	c black",
+"X	s iconColor2	m white	c white",
+"o	c #C0C0C0C0C0C0",
+"O	s iconGray6	m black	c #636363636363",
+"+	c #808080808080",
+"@	s iconGray5	m black	c #737373737373",
+/* pixels */
+"                ",
+"                ",
+"      .XX.      ",
+"  .X..ooo..XX.. ",
+" XXXXXXXXXXXXXO ",
+".oooooooooooooO ",
+"......XX....... ",
+"....o..oo+....O ",
+"...o.@@.+.....O ",
+"....@X@@@o....O ",
+"....@@@@@o....O ",
+"....@@@@@o....O ",
+"...o.@@.+...... ",
+" ...+oo+......  ",
+"                ",
+"                "};
