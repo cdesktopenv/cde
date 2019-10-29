0f36a57effa0d8cc028e60bd83676771d26e7760
Chase <nicetrynsa@protonmail.ch>
2019-10-28 20:27:35 -0500

    remove more internal jpeg headers

--- cde/lib/DtHelp/jpeg/cdjpeg.h
+++ cde/lib/DtHelp/jpeg/cdjpeg.h
@@ -34,10 +34,8 @@
 
 #define JPEG_CJPEG_DJPEG	/* define proper options in jconfig.h */
 #define JPEG_INTERNAL_OPTIONS	/* cjpeg.c,djpeg.c need to see xxx_SUPPORTED */
-#include "jinclude.h"
 #include <jpeglib.h>
 #include <jerror.h>		/* get library error codes too */
-#include "cderror.h"		/* get application-specific error codes */
 
 
 /*
